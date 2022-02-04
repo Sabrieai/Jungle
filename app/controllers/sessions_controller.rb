b250d7d812a7471802e732cb87b6ec8ed6f17bed
Aisha Sabrie <sabriealt@gmail.com>
2022-02-03 22:55:49 -0500

    conditionally render register and login, logout not working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to [:root]
     else
       redirect_to [:new_session]
     end
