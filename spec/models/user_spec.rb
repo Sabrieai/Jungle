81c9cd6311439bb04b2ab109820d85d1a22e9838
Aisha Sabrie <sabriealt@gmail.com>
2022-02-07 22:25:58 -0500

    add minimum password length, update models and test accordingly

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
 
     it "should save when provided all info and password and confirmation match" do
-      @user = User.create(first_name: "John",last_name:"Crowley", email: "Jo@hn.com", password: "pepe", password_confirmation: "pepe")
+      @user = User.create(first_name: "John",last_name:"Crowley", email: "Jo@hn.com", password: "pepe-the-frog", password_confirmation: "pepe-the-frog")
 
       expect(@user.id).to be_present
     end
@@ -16,13 +16,13 @@ RSpec.describe User, type: :model do
     end
 
     it "shouldn't save without a name" do
-      @user = User.create(first_name: nil,last_name:"Crowley", email: "Jo@hn.com", password: "pepe", password_confirmation: "peep")
+      @user = User.create(first_name: nil,last_name:"Crowley", email: "Jo@hn.com", password: "pepe-the-frog", password_confirmation: "peep-the-frog")
 
       expect(@user.id).to be_nil
     end
 
     it "shouldn't save without a email" do
-      @user = User.create(first_name: "John",last_name: nil, email: "Jo@hn.com", password: "pepe", password_confirmation: "peep")
+      @user = User.create(first_name: "John",last_name: nil, email: "Jo@hn.com", password: "pepe-the-frog", password_confirmation: "peep-the-frog")
 
       expect(@user.id).to be_nil
     end
@@ -33,22 +33,28 @@ RSpec.describe User, type: :model do
       expect(@user.id).to be_nil
     end
 
+    it "shouldn't save with a too short password" do
+      @user = User.create(first_name: "John",last_name:"Crowley", email: "Jo@hn.com", password: "short", password_confirmation: "short")
+
+      expect(@user.id).to be_nil
+    end
+
     it "shouldn't save without confirmation" do
-      @user = User.create(first_name: "John",last_name:"Crowley", email: "Jo@hn.com", password: "pepe", password_confirmation: nil)
+      @user = User.create(first_name: "John",last_name:"Crowley", email: "Jo@hn.com", password: "pepe-the-frog", password_confirmation: nil)
 
       expect(@user.id).to be_nil
     end
 
     it "Should save when email is unique" do
-      @user1 = User.create(first_name: "Buster",last_name:"Zippa", email: "buzz@cuts.com", password: "zzz", password_confirmation: "zzz")
-      @user2 = User.create(first_name: "John",last_name:"Crowley", email: "Jo@hn.com", password: "pepe", password_confirmation: "pepe")
+      @user1 = User.create(first_name: "Buster",last_name:"Zippa", email: "buzz@cuts.com", password: "zzzsleep", password_confirmation: "zzzsleep")
+      @user2 = User.create(first_name: "John",last_name:"Crowley", email: "Jo@hn.com", password: "pepe-the-frog", password_confirmation: "pepe-the-frog")
 
       expect(@user2.id).to be_present
     end
 
     it "Shouldn't save when email is not unique" do
-      @user1 = User.create(first_name: "Buster",last_name:"Zippa", email: "buzz@cuts.com", password: "zzz", password_confirmation: "zzz")
-      @user2 = User.create(first_name: "John",last_name:"Crowley", email: "Buzz@CUTS.com", password: "pepe", password_confirmation: "pepe")
+      @user1 = User.create(first_name: "Buster",last_name:"Zippa", email: "buzz@cuts.com", password: "zzzsleep", password_confirmation: "zzzsleep")
+      @user2 = User.create(first_name: "John",last_name:"Crowley", email: "Buzz@CUTS.com", password: "pepe-the-frog", password_confirmation: "pepe-the-frog")
 
       expect(@user2.id).to be_nil
     end
