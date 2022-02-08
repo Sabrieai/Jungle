f03bc5ebfa5ab4eabcba172472c3511d478c7551
Aisha Sabrie <sabriealt@gmail.com>
2022-02-07 23:39:00 -0500

    add user authenticatin to user model, update sessions-controller accordingly

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 8 }
   validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)  
+    user = find_by_email(email)  
+    if user && user.authenticate(password)
+      user
+    else  
+       nil  
+    end  
+  end  
 end
